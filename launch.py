6e236b724c814761cacf7d204eaca0aa0fa2097c
Lukas Ernst <ernst.lukas@gmx.net>
2021-03-22 14:28:02 +0100

    implemented usage of absolute paths

--- launch.py
+++ launch.py
@@ -2,12 +2,14 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 import runpy
 import sys
+import pathlib
 
 Tk().withdraw()
-
 filename = askopenfilename()
 
+currentPath=pathlib.Path(__file__).parent.absolute() #current folder of launch.py
+analysisPath=currentPath.joinpath('analyse.py')
+
 sys.argv=['', '-s', filename]
-runpy.run_path('./analyse.py', run_name='__main__')
+runpy.run_path(analysisPath, run_name='__main__')
 input()
-#
